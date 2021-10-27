df5f2c78e1791663885059d6a9c32356176b00c9
--local <szymko149@gmail.com>
2021-10-27 17:08:16 +0200

    Remove unnecesary docker run command

--- run_subread.sh
+++ run_subread.sh
@@ -12,9 +12,6 @@ docker run --platform linux/amd64 -it --rm -v $(pwd):/data nanozoo/subread:2.0.2
 mv drosophila_index.00.b.array drosophila_index.00.b.tab drosophila_index.files drosophila_index.log drosophila_index.reads ./drosophila_subread_index
 
 
-docker run --platform linux/amd64 -it --rm -v $(pwd):/data nanozoo/subread:2.0.2--53f5da6 subread-align -T 3 -t 1 -d 50 -D 600 -i /data/drosophila_subread_index/drosophila_index -r /data/bfc_corrected_trimmomatic_SRR6008629_1.fastq.gz -R /data/bfc_corrected_trimmomatic_SRR6008629_2.fastq.gz -o /data/subread_SRR6008629_PE.bam
-
-
 for i in "${!SAMPLE1[@]}"; do
     docker run --platform linux/amd64 -it --rm -v $(pwd):/data nanozoo/subread:2.0.2--53f5da6 subread-align -T 3 -t 1 -d 50 -D 600 -i /data/drosophila_subread_index/drosophila_index -r /data/bfc_corrected_trimmomatic_"${SAMPLE1[i]}".fastq.gz -R /data/bfc_corrected_trimmomatic_"${SAMPLE2[i]}".fastq.gz -o /data/subread_"${SAMPLE1[i]}"_PE.bam
 done
