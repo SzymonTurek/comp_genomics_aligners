0e1d9d9498ab7ff9d7c61636e2229504c65085d6
--local <szymko149@gmail.com>
2021-10-27 16:54:13 +0200

    Add docker run commands to run alignment

--- run_subread.sh
+++ run_subread.sh
@@ -4,13 +4,20 @@ SAMPLE1=(SRR6008648_1 SRR6008629_1)
 SAMPLE2=(SRR6008648_2 SRR6008629_2)
 
 
-mkdir trimmomatic_output
+mkdir subread_output
+mkdir drosophila_subread_index
+
+docker run --platform linux/amd64 -it --rm -v $(pwd):/data nanozoo/subread:2.0.2--53f5da6 subread-buildindex -o /data/drosophila_index /data/GCF_000001215.4_Release_6_plus_ISO1_MT_genomic.fna
+
+mv drosophila_index.00.b.array drosophila_index.00.b.tab drosophila_index.files drosophila_index.log drosophila_index.reads ./drosophila_subread_index
+
+
+docker run --platform linux/amd64 -it --rm -v $(pwd):/data nanozoo/subread:2.0.2--53f5da6 subread-align -T 3 -t 1 -d 50 -D 600 -i /data/drosophila_subread_index/drosophila_index -r /data/bfc_corrected_trimmomatic_SRR6008629_1.fastq.gz -R /data/bfc_corrected_trimmomatic_SRR6008629_2.fastq.gz -o /data/subread_SRR6008629_PE.bam
 
 
 for i in "${!SAMPLE1[@]}"; do
-    docker run --platform linux/amd64 -it --rm -v $(pwd):/data staphb/trimmomatic:0.39 trimmomatic PE /data/"${SAMPLE1[i]}".fastq.gz /data/"${SAMPLE2[i]}".fastq.gz /data/"${SAMPLE1[i]}".trim.fastq.gz /data/"${SAMPLE1[i]}"un.trim.fastq.gz /data/"${SAMPLE2[i]}".trim.fastq.gz /data/"${SAMPLE2[i]}"un.trim.fastq.gz ILLUMINACLIP:/Trimmomatic-0.39/adapters/NexteraPE-PE.fa:2:40:15
+    docker run --platform linux/amd64 -it --rm -v $(pwd):/data nanozoo/subread:2.0.2--53f5da6 subread-align -T 3 -t 1 -d 50 -D 600 -i /data/drosophila_subread_index/drosophila_index -r /data/bfc_corrected_trimmomatic_"${SAMPLE1[i]}".fastq.gz -R /data/bfc_corrected_trimmomatic_"${SAMPLE2[i]}".fastq.gz -o /data/subread_"${SAMPLE1[i]}"_PE.bam
 done
 
-#mv "${SAMPLE1[i]}".trim.fastq.gz "${SAMPLE1[i]}"un.trim.fastq.gz "${SAMPLE2[i]}".trim.fastq.gz "${SAMPLE2[i]}"un.trim.fastq.gz trimmomatic_output/
-mv *.trim.fastq.gz *un.trim.fastq.gz trimmomatic_output/
+mv *.bam *.vcf subread_output/
 
