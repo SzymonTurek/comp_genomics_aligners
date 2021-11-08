271886a4d486ea6359d2f35849d0249614db14ed
--local <szymko149@gmail.com>
2021-11-08 14:18:10 +0100

    Add samtools programs for conversion and stat

---
+++ sam_to_bam.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+SAMPLES="SRR6008648_1 SRR6008629_1"
+
+
+#for sample in ${SAMPLES}; do
+#    docker run --platform linux/amd64 -it --rm -v $(pwd)/bowtie2_output:/data staphb/samtools:1.13 samtools view -@ 3 -bS  /data/${sample}.sam  -o /data/${sample}.bam
+
+#done
+
+
+#for sample in ${SAMPLES}; do
+#    docker run --platform linux/amd64 -it --rm -v $(pwd)/bowtie2_output:/data staphb/samtools:1.13 samtools sort -@ 3 /data/${sample}.bam  -o /data/${sample}_sorted.bam
+
+#done
+
+#for sample in ${SAMPLES}; do
+#    docker run --platform linux/amd64 -it --rm -v $(pwd)/bowtie2_output:/data staphb/samtools:1.13 samtools sort -@ 3 /data/${sample}.bam  -o /data/${sample}_sorted.bam
+
+#done
+
+
+#for sample in ${SAMPLES}; do
+#    docker run --platform linux/amd64 -it --rm -v $(pwd)/bowtie2_output:/data staphb/samtools:1.13 samtools index -@ 3 /data/${sample}_sorted.bam
+
+#done
+
+#for sample in ${SAMPLES}; do
+#    docker run --platform linux/amd64 -it --rm -v $(pwd)/bowtie2_output:/data staphb/samtools:1.13 samtools idxstats -@ 3 /data/${sample}_sorted.bam > ${sample}_idxstats.txt
+
+#done
+
+#mv *.txt bowtie2_output
+
+for sample in ${SAMPLES}; do
+    docker run --platform linux/amd64 -it --rm -v $(pwd)/bowtie2_output:/data staphb/samtools:1.13 samtools stats -@ 3 /data/${sample}_sorted.bam > ${sample}_stats.txt
+
+done
+
+mv *.txt bowtie2_output
